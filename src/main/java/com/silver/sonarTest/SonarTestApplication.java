6263d4af8eb622c7f1155ff0370984e8bc82b4da
Silver Nunez <silver@Silvers-MacBook-Pro.local>
2018-09-19 20:52:19 +0100

    Initial commit

---
+++ src/main/java/com/silver/sonarTest/SonarTestApplication.java
@@ -0,0 +1,12 @@
+package com.silver.sonarTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonarTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonarTestApplication.class, args);
+	}
+}
